b8e69a232c3d973d9c47958f4f37284ea2509e40
517030910405 <lijiasen0921@126.com>
2019-12-12 21:25:26 +0800

    utils.video

--- utils/video.py
+++ utils/video.py
@@ -3,9 +3,9 @@ class ReadVideo:
     def __init__(self, name):
         self.name = name
         self.video = cv2.VideoCapture(name)
-        self.width = self.video.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height = self.video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        self.fps = self.video.get(cv2.CAP_PROP_FPS)
+        self.width = round(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = round(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.fps = round(self.video.get(cv2.CAP_PROP_FPS))
 
     def get_frame(self, i):
         self.video.set(cv2.CAP_PROP_POS_FRAMES, round(i * self.fps))
@@ -18,9 +18,9 @@ class ReadVideo:
 class WriteVideo:
     def __init__(self, name, width, height, fps):
         self.name = name
-        self.width = width
-        self.height = height
-        self.fps = fps
+        self.width = round(width)
+        self.height = round(height)
+        self.fps = round(fps)
         self.video = cv2.VideoWriter(name,cv2.VideoWriter_fourcc(*"DIVX"),fps,(self.width,self.height))
 
     def write_frame(self, frame):
